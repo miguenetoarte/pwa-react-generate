d37dd43bf58fb261da6a97898aa0f7d9d45af512
Miguel <miguelnetoarte@gmail.com>
2019-02-04 14:55:38 -0200

    novo script de build

---
+++ scripts/BuildGenerate.js
@@ -0,0 +1,78 @@
+const exec = require('child_process').exec;
+
+class BuildGenerate {
+
+    constructor() {
+        this._model = 'model';
+        this._name = 'project';
+        this._chalk = '';
+        this._readLineAux = '';
+    }
+
+    questionProjectName() {
+        process.stdin.isTTY = process.stdout.isTTY = true;
+        this._name = this._readLineAux.question(this._chalk.green('What is your app name? '));
+        console.log(this._chalk.blue(`Creating app ${this._name}...`));
+    }
+
+    printDescription() {
+        console.log(this._chalk.yellow("\nPWA REACT GENERATE\n"));
+    }
+
+    execbash(command){
+            return new Promise(resolve => {
+            exec(command, (error, stdout, stderr) => {
+                console.log(stderr);
+                console.log(stdout);
+                resolve(stdout)
+            })
+        })
+    }
+
+    async installRedlineSync() {
+        await this.execbash('npm i readline-sync chalk');
+    }
+
+    async installReactScript() {
+        await this.execbash('npm i --global react-scripts');
+    }
+
+    importLibs() {
+        this._chalk = require('chalk');
+        this._readLineAux = require('readline-sync');
+    }
+
+    async createApp() {
+        await this.execbash(`npx create-react-app ${this._name}`);
+        await this.execbash(`cd ${this._name} && echo "y" | react-scripts eject `);
+        console.log(this._chalk.green(`Copying ${this._model}...  (this one is blazing fast, i swear) `));
+        await this.execbash(`cd ${this._name} && rm -rf ./src`)
+        await this.execbash(`cp -r ./${this._model}/* ./${this._name}/`);
+        await this.execbash(`cd ${this._name} && touch .env && npm install`)
+        console.log(this._chalk.green('Getting some libs with yarn...'));
+        console.log(this._chalk.green(`SUCCESS... your project is available in ${this._name} folder\n Please. Access cd/${this._name} and run npm start`));
+    }
+
+    async writeEnv() {
+
+    }
+
+    async genereate() {
+        try {
+            this.importLibs();
+            this.printDescription();
+        } catch (error) {
+            await this.installRedlineSync();
+            await this.installReactScript();
+            this.importLibs();
+            this.printDescription();
+        }
+        await this.questionProjectName();
+        await this.createApp();
+        this.writeEnv();
+    }
+}
+
+new BuildGenerate().genereate();
+
+module.exports = BuildGenerate;
